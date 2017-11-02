6be476d2f7b5270f4951baf803cbdd2460bd74c1
Ivan Sevcik <dark-raven3@hotmail.com>
2017-11-02 18:09:15 +0100

    RemoteFileSystemModel using StandardItemModel.

--- src/gui/controls/remote_tree_view.py
+++ src/gui/controls/remote_tree_view.py
@@ -8,9 +8,4 @@ from src.logic.remote_file_system_model import RemoteFileSystemModel
 class RemoteTreeView(TransferTreeView):
     def __init__(self, parent=None):
         super().__init__(parent)
-        model = QStandardItemModel()
-        model.setHorizontalHeaderLabels(["Hi"])
-        q = QStandardItem("Hello")
-        model.appendRow(q)
-        q.appendRow(QStandardItem("Hello"))
-        self.setModel(model)
+        self.setModel(RemoteFileSystemModel())
